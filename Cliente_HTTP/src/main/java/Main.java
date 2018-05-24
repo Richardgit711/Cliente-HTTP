80273a4110538bb3c3c8cd19b33835995b8f6fa8
Richardgit711 <39571398+Richardgit711@users.noreply.github.com>
2018-05-24 17:42:11 -0300

    Add files via upload

--- Cliente_HTTP/src/main/java/Main.java
+++ Cliente_HTTP/src/main/java/Main.java
@@ -3,51 +3,54 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       // Validate.isTrue(args.length == 1, "usage: supply url to fetch");
-        String url = "http://news.ycombinator.com/";
-        print("Fetching %s...", url);
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            print("Escriba una URL Valida:");
+            String url = scanner.nextLine();
+            print("Fetching %s...", url);
 
-        Document doc = Jsoup.connect(url).get();
-        Elements forms = doc.select("form");
-        Elements imagenes = doc.select("img");
-        Elements parafos = doc.select("p");
-        //doc.outputSettings(new Document.OutputSettings().prettyPrint(false));
-        String lineas = doc.toString();
+            Document doc = Jsoup.connect(url).get();
+            Elements forms = doc.select("form");
+            Elements imagenes = doc.select("img");
+            Elements parafos = doc.select("p");
+            //doc.outputSettings(new Document.OutputSettings().prettyPrint(false));
+            String lineas = doc.toString();
 
-        print("Lineas: (%d)", lineas.split("\r\n|\r|\n").length);
+            print("Lineas: (%d)", lineas.split("\r\n|\r|\n").length);
 
-        print("\nImagenes: (%d)", imagenes.size());
+            print("\nImagenes: (%d)", imagenes.size());
 
-        print("\nParafos: (%d)", parafos.size());
+            print("\nParrafos: (%d)", parafos.size());
 
-        print("\nFormularios por metodo GET: (%d)", doc.select("form[method=get]").size());
+            print("\nFormularios por metodo GET: (%d)", doc.select("form[method=get]").size());
 
-        print("\nFormularios por metodo POST: (%d)\n", doc.select("form[method=post]").size());
+            print("\nFormularios por metodo POST: (%d)\n", doc.select("form[method=post]").size());
 
-        for (Element form : forms) {
-            print("Formulario: <%s>  (%s)", form.attr("method"), trim(form.text(), 40));
-            if(form.attr("method").contentEquals("post")){
-                Jsoup.connect(form.attr("action"))
-                        .data("asignatura","practica1")
-                        .header("matricula","2012-1039")
-                        .post();
-                print("Respuesta: <%d> (%s)",
-                        Jsoup.connect(form.attr("action")).response().statusCode(),
-                        Jsoup.connect(form.attr("action")).response().statusMessage());
+            for (Element form : forms) {
+                print("Formulario: <%s>  (%s)", form.attr("method"), trim(form.text(), 40));
+                if (form.attr("method").contentEquals("post")) {
+                    Jsoup.connect(url)
+                            .data("asignatura", "practica1")
+                            .header("matricula", "2012-1039")
+                            .post();
+                    print("Respuesta: <%d> (%s)",
+                            Jsoup.connect(url).response().statusCode(),
+                            Jsoup.connect(url).response().statusMessage());
 
+                }
+                Elements inputs = form.select("input");
+                for (Element input : inputs) {
+                    print(" * Inputs: nombre=(%s)  type=(%s)", input.attr("name"), input.attr("type"));
+                }
             }
-            Elements inputs = form.select("input");
-            for(Element input: inputs){
-                print(" * Inputs: nombre=(%s)  type=(%s)", input.attr("name"), input.attr("type"));
-            }
+
+
         }
-
-
-
     }
 
     private static void print(String msg, Object... args) {
